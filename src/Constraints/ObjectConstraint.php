51cbbd9a7e0235ef226296301fbc7f9ae8b8a266
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2019-10-13 00:37:35 +0200

    Runs on HHVM 4.27

--- src/Constraints/ObjectConstraint.php
+++ src/Constraints/ObjectConstraint.php
@@ -16,16 +16,10 @@ class ObjectConstraint {
       $input = JsonSchema\json_decode_hack($input);
     }
 
-    # This will first attempt to assert that the incoming input is a `dict`. We
-    # can't use `coerceType` from `DictSpec` here because given a dict-like
-    # PHP array, it would successfully coerce it into a dict. For JSON
-    # schema, that should be considered invalid: passing an array when we
-    # expect an object is an invalid input.
     $spec = TypeSpec\dict(TypeSpec\string(), TypeSpec\mixed());
-    try {
-      return $spec->assertType($input);
-    } catch (TypeAssert\TypeCoercionException $e) {
 
+    // TypeAssert 3.6.2 workaround. Will be moved back as soon as possible.
+    $fallback_func = () ==> {
       # Fallback to checking legacy PHP dict-like-arrays
       $fallback = TypeSpec\dict_like_array(TypeSpec\string(), TypeSpec\mixed());
       try {
@@ -46,6 +40,20 @@ class ObjectConstraint {
         );
         throw new JsonSchema\InvalidFieldException($pointer, vec[$error]);
       }
+    };
+
+    # This will first attempt to assert that the incoming input is a `dict`. We
+    # can't use `coerceType` from `DictSpec` here because given a dict-like
+    # PHP array, it would successfully coerce it into a dict. For JSON
+    # schema, that should be considered invalid: passing an array when we
+    # expect an object is an invalid input.
+    try {
+      if($input is dict<_, _>){
+        return $spec->assertType($input);
+      }
+      return $fallback_func();
+    } catch (TypeAssert\TypeCoercionException $e) {
+      return $fallback_func();
     } catch (TypeAssert\IncorrectTypeException $e) {
       $error = shape(
         'code' => JsonSchema\FieldErrorCode::INVALID_TYPE,
