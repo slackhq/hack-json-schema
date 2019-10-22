71de486fa5fe435c6725f26029e50db4922c1a06
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2019-10-22 16:12:03 +0200

    Make the test pass

--- src/BaseValidator.php
+++ src/BaseValidator.php
@@ -7,8 +7,9 @@ use namespace HH\Lib\C;
 <<__ConsistentConstruct>>
 abstract class BaseValidator<+T> implements Validator<T> {
   private vec<TFieldError> $errors = vec[];
-  private ?T $validated_input;
-  private bool $validated = false;
+  <<__LateInit>> private T $validated_input;
+  private bool $has_been_validated = false;
+  private bool $is_invalid = false;
 
   public function __construct(protected mixed $input, protected string $pointer = '') {}
 
@@ -17,9 +18,11 @@ abstract class BaseValidator<+T> implements Validator<T> {
   final public function validate(): void {
     try {
       $this->validated_input = $this->process();
-      $this->validated = true;
+      $this->has_been_validated = true;
+      $this->is_invalid = false;
     } catch (InvalidFieldException $e) {
       $this->errors = $e->errors;
+      $this->is_invalid = true;
     }
   }
 
@@ -32,10 +35,10 @@ abstract class BaseValidator<+T> implements Validator<T> {
   }
 
   final public function getValidatedInput(): T {
-    if ($this->validated_input === null) {
-      if (!$this->validated) {
-        throw new \Exception('Must call `validate` before accessing validated input.');
-      }
+    if (!$this->has_been_validated) {
+      throw new \Exception('Must call `validate` before accessing validated input.');
+    }
+    if ($this->is_invalid) {
       throw new \Exception("Can't access validated input since validator is invalid.");
     }
 
