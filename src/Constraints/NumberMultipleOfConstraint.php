2d3f4f5e8db4259229f644c3b1ba339743a65292
Lexidor Digital <31805625+lexidor@users.noreply.github.com>
2020-02-08 18:44:54 +0100

    Add some comments and remove a var_dump()

--- src/Constraints/NumberMultipleOfConstraint.php
+++ src/Constraints/NumberMultipleOfConstraint.php
@@ -5,6 +5,12 @@ namespace Slack\Hack\JsonSchema\Constraints;
 use namespace HH\Lib\Math;
 use namespace Slack\Hack\JsonSchema;
 
+// Because comparing floating point numbers using `===` will have undesierable results,
+// we will compare with a little bit of leeway.
+// We are looking for a remainer of 0 after the modulo.
+// `30 % 3` is 0, so 30 is a multiple of 3.
+// `fmod(6., .6)` is not 0, because of floating point rounding errors.
+// It is 2.220...E-16, but this is almost zero, so we pass the test.
 const float COMPARISON_LEEWAY = 10. ** -6;
 
 class NumberMultipleOfConstraint {
@@ -14,14 +20,6 @@ class NumberMultipleOfConstraint {
     $remainer = Math\abs(
       $dividend is int && $devisor is int ? $dividend % $devisor : \fmod((float)$dividend, (float)$devisor),
     );
-    \var_dump(dict[
-      'div' => $dividend,
-      'dev' => $devisor,
-      'res' => $remainer,
-      'rem min dev' => $remainer - $devisor,
-      'left' => $remainer > (1 * 10 ** -6),
-      'right' => ($remainer - $devisor) > (1 * 10 ** -6),
-    ]);
 
     $error = shape(
       'code' => JsonSchema\FieldErrorCode::FAILED_CONSTRAINT,
@@ -37,8 +35,12 @@ class NumberMultipleOfConstraint {
       throw new JsonSchema\InvalidFieldException($pointer, vec[$error]);
     }
     if ($remainer is float) {
+      // If we are closer to 0 than to the devisor, we check assert that our remainer
+      // is less than our COMPARISON_LEEWAY.
       if ($remainer < $devisor / 2 && $remainer > COMPARISON_LEEWAY) {
         throw new JsonSchema\InvalidFieldException($pointer, vec[$error]);
+        // However, sometimes the remainer is very close to the devisor.
+        // That could also indicate a multiple of the devisor.
       } else if ($remainer > $devisor / 2 && ($devisor - $remainer) > COMPARISON_LEEWAY) {
         throw new JsonSchema\InvalidFieldException($pointer, vec[$error]);
       }
