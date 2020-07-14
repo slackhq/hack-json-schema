a56c896696c91e7297c304145784f073deb88c86
Andrew MacDonald <macdonald.andrew@gmail.com>
2020-07-14 13:22:14 -0700

    Support form-encoded uncoerced non-string arrays.
    The existing `Str/split` results in string-typed output items,
    regardless of the format of the input items. If the item parsing is not
    performed in "coerced" mode, this will fail for non-string items.
    
    Relying on json_decode_hack allows form-encoded uncoerced non-string
    arrays to be parsed as intended.

--- src/Constraints/ArrayConstraint.php
+++ src/Constraints/ArrayConstraint.php
@@ -22,7 +22,9 @@ class ArrayConstraint {
       // check to see if we can create an array from a comma delimited string.
       $valid_json = $coerced is nonnull && $coerced is Traversable<_>;
       if (!$valid_json) {
-        $coerced = Str\split($input, ',');
+        // Wrap in brackets and try JSON decoding again. This'll result in
+        // correctly typed output for non-string form encoded arrays.
+        $coerced = JsonSchema\json_decode_hack("[{$input}]");
       }
 
       $input = $coerced;
