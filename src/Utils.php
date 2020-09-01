393ba7b6a41b4d31b1d531a4c993f4fd3a60f265
Michael Hahn <mh@slack-corp.com>
2020-09-01 10:18:27 -0700

    Remove HHVM_VERSION_ID check

--- src/Utils.php
+++ src/Utils.php
@@ -13,17 +13,8 @@ function get_pointer(string $current, string ...$parts): string {
 
 function get_function_name_from_function(mixed $function): string {
   $func_name = '';
-
-  if (\HHVM_VERSION_ID >= 42100) {
-    /*HH_FIXME[2049]*//*HH_FIXME[4107] These errors don't apply in hhvm > 4.21*/
-    $is_function = \is_callable_with_name($function, false, inout $func_name);
-    invariant($is_function, 'You may only pass named functions to %s', __FUNCTION__);
-  } else {
-    /*HH_FIXME[4105] The error is not relevant for hhvm < 4.21*/
-    $is_function = \is_callable($function, false, inout $func_name);
-    invariant($is_function, 'You may only pass named functions to %s', __FUNCTION__);
-  }
-
+  $is_function = \is_callable_with_name($function, false, inout $func_name);
+  invariant($is_function, 'You may only pass named functions to %s', __FUNCTION__);
   return $func_name;
 }
 
